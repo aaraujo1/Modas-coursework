b26827bd2c643655b40f1c9fd0d0ffb1e4f8c745
André Araujo <aaraujo1@my.wctc.edu>
2019-02-11 18:48:40 -0600

    API GET - All Events
    1) Create a new controller EventController
    2) Define a custom route for the controller: api/event
    3) Create a controller method to expose all events

---
+++ Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Modas.Models;
+
+namespace Modas.Controllers
+{
+    //api/event in controller
+    [Route("api/[controller]")]
+    public class EventController : Controller
+    {
+        private IEventRepository repository;
+        public EventController(IEventRepository repo) => repository = repo;
+
+        [HttpGet]
+        // returns all events (unsorted)
+        //lazy loading
+        public IEnumerable<Event> Get() => repository.Events
+            .Include(e => e.Location);
+
+    }
+}
