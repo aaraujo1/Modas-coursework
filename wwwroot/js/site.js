2ec20b6eef87b29c83c6c5ae679af1eb99296f85
André Araujo <aaraujo1@my.wctc.edu>
2019-02-18 19:04:32 -0600

    API Consumer - update client.js
    1) Use jQuery getJSON() to retrieve first page of data
    2) Display data using the console

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,7 +3,22 @@
      $(function() {
         //disableFirst();
         //disableLast();
-         $('#buttonFirst').prop('disabled', true).;
+        // $('#buttonFirst').prop('disabled', true).;
 
-        alert('test');
+        //alert('test');
+
+getEvents(1)
+    
+        function getEvents(page) {
+            $.getJSON({
+                url: "../api/event/page" + page,
+                success: function (response, textStatus, jqXhr) {
+                    console.log(response);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    // log the error to the console
+                    console.log("The following error occured: " + textStatus, errorThrown);
+                }
+            });
+        }
     });
